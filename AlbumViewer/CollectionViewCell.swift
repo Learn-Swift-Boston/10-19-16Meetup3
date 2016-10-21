7ab93957d2925620d2c07d949fd75554c88a3db1
Matt Dias <mdias@zipcar.com>
2016-10-20 22:11:51 -0400

    uploaded original state of Album Viewer and a read me detailing the 10/16 meetup

---
+++ AlbumViewer/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  AlbumViewer
+//
+//  Created by Matt Dias on 10/19/16.
+//
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+	@IBOutlet weak var titleLabel: UILabel!
+}
