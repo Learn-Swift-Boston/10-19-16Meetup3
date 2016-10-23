bf2377d3bb90bd6b86caa33d81049a62fd3b978c
Matt Dias <matt.dias6@gmail.com>
2016-10-23 15:00:18 -0400

    created cell, cell xib, and added default image

---
+++ AlbumViewer/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  AlbumViewer
+//
+//  Created by Matt Dias on 10/23/16.
+//
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
