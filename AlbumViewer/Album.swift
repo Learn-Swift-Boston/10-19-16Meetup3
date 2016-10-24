cd778cfee6742d3aa360d095d07391242eb44610
Matt Dias <matt.dias6@gmail.com>
2016-10-23 20:22:50 -0400

    created object

---
+++ AlbumViewer/Album.swift
@@ -0,0 +1,29 @@
+//
+//  Album.swift
+//  AlbumViewer
+//
+//  Created by Matthew Dias on 10/23/16.
+//
+//
+
+import Foundation
+
+
+struct Album {
+    var title: String
+    var year: String
+    var artWorkUrlString: String
+
+
+    init?(json: [String: Any]) {
+        guard let title = json["collectionNme"] as? String,
+              let dateString = json["releaseDate"] as? String,
+              let artWorkUrlString = json["artworkUrl100"] as? String else {
+                return nil
+        }
+
+        self.title = title
+        self.year = dateString.substring(to: dateString.index(dateString.startIndex, offsetBy: 3))
+        self.artWorkUrlString = artWorkUrlString
+    }
+}
