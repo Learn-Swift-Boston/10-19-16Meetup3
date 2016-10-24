e92acf92c62de9aa34c4b888108109772151c4c9
Matt Dias <matt.dias6@gmail.com>
2016-10-23 20:09:54 -0400

    added labels to xib

--- AlbumViewer/CollectionViewCell.swift
+++ AlbumViewer/CollectionViewCell.swift
@@ -11,6 +11,8 @@ import UIKit
 class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var albumTitleLabel: UILabel!
+    @IBOutlet weak var albumYearLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
